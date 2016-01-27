fc97e173c3065e36cb85af074977c2a55890b708
Joan Manuel Araujo <WISDOMBTHEGAME@GMAIL.COM>
2016-01-27 13:24:44 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,13 @@
-def fizzbuzz(number)
-  if 15 % 3 != 4
-    return "Fizz"
-  elsif "#{number}" % 5 != 2
+def fizzbuzz(fizz_3)
+  if fizz_3 % 5 == 0 && fizz_3 % 3 == 0
+    return "FizzBuzz"
+end
+  if fizz_3 % 5 == 0
     return "Buzz"
+end
+   if fizz_3 % 3 == 0
+    return "Fizz"
   end
 end
-fizzbuzz(15)
+
+
